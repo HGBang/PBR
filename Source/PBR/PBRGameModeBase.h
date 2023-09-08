94b03ef27d750b564ac2676e03235259c31c1826
HGbang <koko9797w@naver.com>
2023-09-08 17:36:02 +0900

    Project Base Commit

---
+++ Source/PBR/PBRGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PBRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PBR_API APBRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
